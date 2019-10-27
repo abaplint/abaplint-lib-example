ee9a309d693762e74084ceb8fa206778d75eaec4
larshp <larshp@hotmail.com>
2019-10-27 13:29:56 +0100

    update example

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,9 @@
 import * as abaplint from "abaplint";
-import {MemoryFile} from "abaplint/build/src/files";
 
 function run() {
 
   const reg = new abaplint.Registry();
-  const file = new MemoryFile("zfoobar.prog.abap", "REPORT zfoob.\n");
+  const file = new abaplint.MemoryFile("zfoobar.prog.abap", "REPORT zfoob.\n");
   reg.addFile(file);
   console.dir(reg.findIssues());
 
